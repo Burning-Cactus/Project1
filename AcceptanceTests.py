817005ef1460c921f093d6417945903aa38a591b
MajorFubar <trolle3.14@gmail.com>
2019-03-12 17:39:33 -0500

    some stuff *shrug*

--- AcceptanceTests.py
+++ AcceptanceTests.py
@@ -2,7 +2,6 @@ import unittest
 import User
 
 class AcceptanceTests(unittest.TestCase):
-    {
     def test_things(self):
         self.assertEquals(CreateAccount("1, AKelly, password, Null, Null, Andrew, Kelly, 262-262-2626, 414-414-4141, 5698"),"Account successfully created.");
         user = User;
@@ -12,5 +11,4 @@ class AcceptanceTests(unittest.TestCase):
         self.assertEquals(DeleteAccount(user.permission, user2.databaseID), user2.username+"'s account has been deleted.");
         user3 = User
         self.assertEquals(EditAccount(user.permission, user3), "Account has been edited.")
-        self.assertEquals()
-    }
+        self.assertEquals(CreatCourse("CourseName, CourseNumber", "Course created."))
