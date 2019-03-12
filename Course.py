e2b37a24a62ce2e341519c24caf7cade79bfb98c
Archie <Archiez0077@gmail.com>
2019-03-12 12:43:12 -0500

    Added some methods to test

--- Course.py
+++ Course.py
@@ -1,8 +1,31 @@
 
 
 class Course:
+    databaseID = 0
+    name = ""
+    courseNumber = 0
+    classNumber = 0
+    labList = []
+    time = ""
+    location = ""
 
-    def __setDataBaseID__(value):
+
+    def setDataBaseID(self, value):
         # Code
-        return
+        return 0
 
+    def getDataBaseID(self, value):
+        # Code
+        return 0
+
+    def Create(self):
+        # code
+        return 0
+
+    def fromString(self):
+        # code
+        return 0
+
+    def updateDataBase(self):
+        # code
+        return 0
--- User.py
+++ User.py
@@ -7,6 +7,25 @@ class User:
     def __methodName__(self):
         # Format for stubs
         return 0
+    def setDataBaseID(self, value):
+        # Code
+        return 0
+
+    def getDataBaseID(self, value):
+        # Code
+        return 0
+
+    def Create(self):
+        # code
+        return 0
+
+    def fromString(self):
+        # code
+        return 0
+
+    def updateDataBase(self):
+        # code
+        return 0
 
 
 
