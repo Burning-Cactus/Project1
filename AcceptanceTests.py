1dae732f76f7f06e8ae27dc042800fbaa708bf9e
MajorFubar <trolle3.14@gmail.com>
2019-03-12 16:18:20 -0500

    First few

---
+++ AcceptanceTests.py
@@ -0,0 +1,15 @@
+import unittest
+import User
+
+class AcceptanceTests(unittest.TestCase):
+    {
+    def test_things(self):
+        self.assertEquals(CreateAccount("1, AKelly, password, Null, Null, Andrew, Kelly, 262-262-2626, 414-414-4141, 5698"),"Account successfully created.");
+        user = User;
+        user.permission = 1;
+        user2 = User;
+        user2.databaseID = 45;
+        self.assertEquals(DeleteAccount(user.permission, user2.databaseID), user2.username+"'s account has been deleted.");
+        self.assertEquals
+
+    }
