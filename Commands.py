e4df29d56d51bf43e2bb1eb0a9069b7dc08c2a4c
MajorFubar <trolle3.14@gmail.com>
2019-03-12 20:05:13 -0500

    more stuff, I hate weirdly worded math problems

---
+++ Commands.py
@@ -0,0 +1,12 @@
+
+
+class Commands:
+
+    def CreateAccount(self):
+        return"";
+    def EditAccount(self):
+        return"";
+    def DeleteAccount(self):
+        return"";
+    def CreateCourse(self):
+        return"";
